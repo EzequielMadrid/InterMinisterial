326befb218c80ad73cefb5e7e03173cdbb06aa36
Ezequiel Madrid <madridezequiel299@gmail.com>
2025-11-12 02:08:14 -0300

    auth and sync

---
+++ src/actions/user.action.ts
@@ -0,0 +1,32 @@
+"use server";
+
+import { prisma } from "@/lib/prisma";
+import { auth, currentUser } from "@clerk/nextjs/server";
+
+export async function syncUser() {
+  // Logic to sync user data with the database
+  try {
+    const { userId } = await auth();
+    const user = await currentUser();
+    if (!userId || !user) return;
+    // Check if user already exists
+    const existingUser = await prisma.user.findUnique({
+      where: { clerkId: userId },
+    });
+    if (existingUser) return existingUser;
+    // Creating new user
+    const dbUser = await prisma.user.create({
+      data: {
+        clerkId: userId,
+        name: `${user.firstName || ""} ${user.lastName || ""}`,
+        username:
+          user.username ?? user.emailAddresses[0].emailAddress.split("@")[0],
+        email: user.emailAddresses[0].emailAddress,
+        image: user.imageUrl,
+      },
+    });
+    return dbUser;
+  } catch (error) {
+    console.error("Error syncing user:", error);
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import ModeToggle from "@/components/ModeToggle";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
+    // Forum page
     <div className="m-6 flex items-center justify-around font-mono">
       <h1>FORO</h1>
     </div>
